d85e696343b74549ea302fc89ed71a7eaf65f109
Helltar <iam@helltar.com>
2022-11-19 08:51:11 +0200

    add. license form

---
+++ src/forms/ulicenseform.pas
@@ -0,0 +1,22 @@
+unit uLicenseForm;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Forms, StdCtrls;
+
+type
+
+  { TfrmLicense }
+
+  TfrmLicense = class(TForm)
+    memLicense: TMemo;
+  end;
+
+implementation
+
+{$R *.lfm}
+
+end.
